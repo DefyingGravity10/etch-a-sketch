76fbddab4a08abca425baf177212c6a064c61b2e
Jeanne <jeanneclarissetoledo@gmail.com>
2022-08-09 11:15:03 +0800

    Set up a hover effect which allows users to draw on the board

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,15 @@
+function changeColor(e) {
+    e.target.style.background = "black";
+}
+
 const container = document.querySelector(".container");
 
 for (let i=0; i<256; i++) {
     const div = document.createElement("div");
-    div.classList.add('content');
+    div.classList.add("content");
     div.textContent = "";
     container.appendChild(div);
 }
+
+const squares = Array.from(document.querySelectorAll(".content"));
+squares.forEach(square => square.addEventListener("mouseover", changeColor));
